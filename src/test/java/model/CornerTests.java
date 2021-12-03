6038b67e70b76252e861685bce502ed69fc7d751
maxglaida <maxglaida@gmail.com>
2021-12-03 15:59:43 +0100

    mock for CornerTest added

--- src/test/java/model/CornerTests.java
+++ src/test/java/model/CornerTests.java
@@ -2,13 +2,17 @@ package model;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 
 public class CornerTests {
 
     @Test
     public void createCorner() {
-        Corner corner = new Corner(Team.GUEST, new MatchTime(2), "test-player");
+        MatchTime matchTime = Mockito.mock(MatchTime.class);
+        Mockito.when(matchTime.getBasetime()).thenReturn(0);
+        Corner corner = new Corner(Team.GUEST, matchTime, "test-player");
         Assertions.assertEquals(corner.getTeam(), Team.GUEST);
     }
 
